d14d04fffce7de23fd4962be73c55675b0b90401
dakota002 <dakota.d@live.com>
2017-06-28 20:59:10 -0400

    Some more randomness, or not

--- Python/Heightmap.py
+++ Python/Heightmap.py
@@ -1,27 +1,54 @@
 import png, sys
 from datetime import datetime
 from noise import pnoise2, snoise2
+import random as rnd
 
 
 # For a world, octave at 8, freq at 256 * octave seems good,
-# but its fun to play with these settings
-octaves = 8
+# but its fun to play with these
+octaves = rnd.randint(1,8)
+size = 2**rnd.randint(8,12)
+print(sys.argv)
+
+if len(sys.argv) > 2:
+    octaves = int(sys.argv[2])
+    size = int(sys.argv[1])
+elif len(sys.argv) == 2:
+    size = sys.argv[1]
+
 freq = 256.0 * octaves
 rows = []
 
-now = datetime.now().isoformat().replace(":","").replace(".","")+"-"+str(freq)+"-frequency"+"-"+str(octaves)+"-Octaves"+"-Image.png"
+now = datetime.now().isoformat().replace(":","").replace(".","")+"-"+str(freq)+"-frequency-"+str(octaves)+"-Octaves-snoise-Image.png"
 
+num = rnd.random() * rnd.randint(0,999)
 
-
-for i in range(4096):
+print("The snoise function")
+for i in range(size):
     row = []
-    for j in range(4096):
-        row.append(int(snoise2(j / freq, i / freq, octaves) * 32767.0 + 32768.0))
+    for j in range(size):
+        row.append(int(snoise2(j / freq, i / freq, octaves, base=num) * 32767.0 + 32768.0))
     rows.append(row)
 
 print(now)
 
 with open(now,'wb') as file:
-    w = png.Writer(4096,4096,greyscale=True, bitdepth=16)
+    w = png.Writer(size,size,greyscale=True, bitdepth=16)
     w.write(file,rows)
-    
+
+"""
+print("The pnoise function")
+row=[]
+now = datetime.now().isoformat().replace(":","").replace(".","")+"-"+str(freq)+"-frequency-"+str(octaves)+"-Octaves-pnoise-Image.png"
+for i in range(4096):
+    row = []
+    for j in range(4096):
+        row.append(int(pnoise2(j / freq, i / freq, octaves) * 32767.0 + 32768.0))
+    rows.append(row)
+
+print(now)
+
+with open(now,'wb') as file:
+    w = png.Writer(8192,8192,greyscale=True, bitdepth=16)
+    w.write(file,rows)
+"""
